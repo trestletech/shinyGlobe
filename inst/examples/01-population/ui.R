2bc8dafc101e0fbd3750ecc04c1bc00806dc5711
trestletech <jeff.allen@trestletechnology.net>
2013-11-16 10:46:01 -0600

    Added credits.

--- inst/examples/01-population/ui.R
+++ inst/examples/01-population/ui.R
@@ -4,5 +4,15 @@ library(shinyGlobe)
 #' Define UI for application that demonstrates a simple Ace editor
 #' @author Jeff Allen \email{jeff@@trestletech.com}
 shinyUI(
-  globeOutput("globe")  
+  tagList(
+    globeOutput("globe"),
+    div(id="info", tagList(
+      HTML(
+        'Population data from <a href="http://maxmind.com">MaxMind</a>.'
+      ),
+      HTML(
+        '<br />Built in <a href ="http://rstudio.com/shiny/">Shiny</a> using the <a href ="http://github.com/trestletech/shinyGlobe/">ShinyGlobe</a> package.'
+      )
+    ))
+  )  
 )
